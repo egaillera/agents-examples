3662a155cece752edf4dc6107c3ff9f2684f7692
Enrique Garcia Illera <egaillera@gmail.com>
2024-10-26 13:26:45 +0200

    [fix] translate titles to Engish

--- geometry/app.py
+++ geometry/app.py
@@ -6,8 +6,8 @@ from geometry_agent import create_main_agent
 from langchain_community.callbacks import get_openai_callback
 
 with st.sidebar:
-        st.subheader("Configuración")
-        if st.button("Reiniciar"):
+        st.subheader("Configuration")
+        if st.button("Restart"):
             st.session_state.main_agent = create_main_agent()
             streamlit_js_eval(js_expressions="parent.window.location.reload()")
             
